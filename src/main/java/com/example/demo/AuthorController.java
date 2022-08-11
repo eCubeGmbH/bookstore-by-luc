5ad9bb1be2876dea02e2e719a0a7b33119831cca
Adel Kharbout <80059720+AdelKharbout@users.noreply.github.com>
2022-08-11 14:29:14 +0200

    added Map and changed code

--- src/main/java/com/example/demo/AuthorController.java
+++ src/main/java/com/example/demo/AuthorController.java
@@ -10,28 +10,27 @@ import java.util.*;
 @RestController
 @RequestMapping(value = "/api/author", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthorController {
-    private List<Author> authorList = new ArrayList<>();
+    private HashMap<String, Author> authorList = new HashMap<>();
 
     @PostMapping("/iu")
-    public Author addAuthor(@RequestBody Author author) {
+    public Author addAuthor(@RequestBody Author author, String uuid) {
         //add an unique ID to author before saving it to the authorlist | UUID!
         String authorId = UUID.randomUUID().toString();
         author.setId(authorId);
-        authorList.add(author);
+        authorList.put("uuid", author);
         return author;
 
     }
 
     @GetMapping("/iu/{uuid}")
     Author author(@PathVariable String uuid) {
-        for (int i = 0; i < authorList.size(); i++) {
-            Author currentItem = authorList.get(i);
-            if (currentItem.getId().equals(uuid)) {
-                return currentItem;
-            }
-
+        Author author = authorList.get(uuid);
+        if (author.getId().equals(uuid)) { //author.id oder author.getId und warum ?
+            return author;
+        } else {
+            return null;
         }
-        return null;
+
     }
 
 }
