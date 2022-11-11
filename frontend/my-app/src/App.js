e887f58babf97d0bb25c8cc9ff61bc6afd9067da
schulzeluc <luc.schulze@ecube.de>
2022-11-11 16:28:13 +0100

    react app, post method partially implemented

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState} from "react";
+import {Input} from "semantic-ui-react";
 
 const tableStyle = {
     borderCollapse: 'collapse',
@@ -12,15 +13,6 @@ const tdStyle = {
 
 const App = () => {
 
-    // const x = () => {
-    //     xhr.open("GET", url);
-    //     xhr.send();
-    // }
-    // const xhr = new XMLHttpRequest();
-    // const url = "http://localhost:3000/api/authors";
-
-
-
     const refresh = () => {
         fetch('http://localhost:9988/api/authors', {
             method: "GET",
@@ -31,19 +23,8 @@ const App = () => {
         )
             .then(response => response.json())
             .then(json => setRowsData(json));
-        console.log('hello')
+        console.log('hello from getAll')
     }
-
-    // async function f() {
-    //     let jsonResponse = await fetch('http://localhost:9988/api/authors', {
-    //         method: "GET",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         }
-    //     }).json();
-    //     setRowsData(jsonResponse);
-    // }
-
 
     function onUpdateAuthor(updatedAuthor) {
         const updatedAuthors = rowsData.map(
@@ -61,13 +42,13 @@ const App = () => {
         const [isEditing, setIsEditing] = useState(false);
 
         const [edit, setEdit] = useState(
-            [{id: ' ', name: ' ', birthDate: ' ', country: ' '}])
-        }
-
-
+            [{id: ' ', name: ' ', birthDate: ' ', country: ' '}]
+        )
+    }
 
     const [rowsData, setRowsData] = useState(
-        [{id: ' ', name: ' ', birthDate: ' ', country: ' '}])
+        [{id: ' ', name: ' ', birthDate: ' ', country: ' '}]
+    )
 
     const deleteTableRows = () => {
         const rows = [...rowsData];
@@ -87,71 +68,67 @@ const App = () => {
         setRowsData([...rowsData, {id: ' ', name: ' ', country: ' ', birthDate: ' '}])
     }
 
+    const add = () => {
+        fetch('http://localhost:9988/api/authors', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstParam: rowsData.id,
+                    secondParam: <input defaultValue={rowsData.name}/>,
+                    thirdParam: rowsData.country,
+                    fourthParam: rowsData.birthDate
+                })
+            }
+        )   .then(response => response.json())
+            .then(json => setRowsData(json))
+        console.log('hello from add')
+    }
+
 
     const [editedId, setEditedId] = useState('')
 
-    function cancelEdit() {
+    const [editedName, setEditedName] = useState('')
 
-    }
+    const [editedCountry, setEditedCountry] = useState('')
+
+    const [editedBirthDate, setEditedBirthDate] = useState('')
 
     return(
         <div className="container">
             <div className="row">
                 <div className="col-sm-8">
-
-                    <table>
+                    <table style={tableStyle}>
                     <tbody className="table" style={tableStyle}>
-
                         <tr>
                             <th>ID</th>
                             <th>Full Name</th>
                             <th>Date of Birth</th>
                             <th>Country</th>
-                            <th><button  onClick={addTableRows} >+</button></th>
+                            <th><button  onClick={add} >+</button></th>
                             <button onClick={()=>(deleteTableRows(rowsData.id))}>x</button>
                             <button onClick={refresh}>Refresh</button>
                         </tr>
-                        {rowsData.map(rowsData=>
+                        {rowsData.map(rowsData =>
                             <tr key={rowsData.id}>
-                                <td style={tdStyle}>{editedId === rowsData.id ? <input value={rowsData.id}/> : rowsData.id }</td>
-                                <td style={tdStyle}>{rowsData.name}</td>
-                                <td style={tdStyle}>{rowsData.birthDate}</td>
-                                <td style={tdStyle}>{rowsData.country}</td>
+                                <td style={tdStyle}>{rowsData.id}</td>
+                                <td style={tdStyle}>{editedName === rowsData.name ? <input defaultValue={'Max Mustermann'}/> : rowsData.name}</td>
+                                <td style={tdStyle}>{editedBirthDate === rowsData.birthDate ? <input defaultValue={'2022-11-11'}/> : rowsData.birthDate}</td>
+                                <td style={tdStyle}>{editedCountry === rowsData.country ? <input defaultValue={'DE'}/> : rowsData.country}</td>
                                 <td>
-                                    <button onClick={() => {setEditedId(rowsData.id)}}>Edit</button>
-                                    <button onClick={() => {setEditedId('')}}>Cancel</button>
+                                    <button onClick={() => {setEditedId(rowsData.id); setEditedName(rowsData.name); setEditedCountry(rowsData.country); setEditedBirthDate(rowsData.birthDate)}}>Edit</button>
+                                    <button onClick={() => {setEditedId(''); setEditedName(''); setEditedCountry(''); setEditedBirthDate('')}}>Cancel</button>
                                 </td>
                             </tr>
-
                         )}
-                        <td style={tdStyle}>
-                            <input type="text" value={rowsData.name} onChange={(event)=>(handleChange(event))}
-                                   name="fullName" className="form-control"/>
-                        </td>
-
-                        <td style={tdStyle}><input type="text" value={rowsData.id} onChange={(event)=>(handleChange(event))}
-                                                   name="id" className="form-control"/>
-
-                        </td>
-
-                        <td style={tdStyle}><input type="text" value={rowsData.birthDate} onChange={(event)=>(handleChange(event))}
-                                                   name="dateOfBirth" className="form-control"/>
-                        </td>
-
-                        <td style={tdStyle}><input type="text" value={rowsData.country} onChange={(event)=>(handleChange(event))}
-                                                   name="country" className="form-control"/>
-                        </td>
                     </tbody>
                     </table>
                 </div>
             </div>
         </div>
-
     )
 }
-
-
-
-
 
 export default App
